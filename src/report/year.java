39e316d6c6f135b7cbc0bb940ec2a92d290f31d7
hoonho <hoonho0510@gmail.com>
2025-08-08 17:43:48 +0900

    수정

--- src/report/year.java
+++ src/report/year.java
@@ -1,8 +1,10 @@
 package src.report;
 
-public class year {
+import java.util.Scanner;
+
+public class Year {
     public static void main(String[] args) {
-        java.util.Scanner sc = new java.util.Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.print("연도를 입력하세요: ");
         int year = sc.nextInt();
 
